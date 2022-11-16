2f7bb87a739394eb97746b22af231afa7c1f6c21
tsohleDev <mokhemisitsohle@gmail.com>
2022-11-16 12:51:46 +0200

    Add error message tag and styling
    add span tag ontop of button to show the user the error message in bold and red

---
+++ formValidation.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#submit')
+const email = document.querySelector('#email')
+const emailError = document.querySelector(".error");
+
+form.addEventListener("click", (event) => {
+    if (email.validity.typeMismatch) {
+        emailError.textContent = "Please enter a valid email adress in lower case";
+    } else {
+        emailError.textContent = "";
+    }
+});
