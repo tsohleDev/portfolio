1e165df822dd4614c29a10428d1dbb8c8d63070d
tsohleDev <mokhemisitsohle@gmail.com>
2022-11-18 13:13:18 +0200

    fix syntax

--- script.js
+++ script.js
@@ -1,191 +1,129 @@
-const hamburger = document.querySelector("#hamburger");
-const links = document.querySelectorAll(".nav__desktoplink");
-const popUp = document.querySelector(".portfolio__PopUP");
-const closePopUp = document.querySelector("#cross__project");
-const sections = document.querySelectorAll(
-  "header,#headline, #works, #about, #contact, .footer"
-);
-const main = document.querySelector("main");
+const hamburger = document.querySelector('#hamburger');
+const links = document.querySelectorAll('.nav__desktoplink');
+const popUp = document.querySelector('.portfolio__PopUP');
+const closePopUp = document.querySelector('#cross__project');
+const sections = document.querySelectorAll('header,#headline, #works, #about, #contact, .footer');
+const main = document.querySelector('main');
 
 function invisibleToogle() {
-  const menu = document.querySelector(".closed__menu");
-  const newMenu = document.querySelector(".opened__menu");
-  menu.classList.replace("closed_menu", "opened_menu");
-  newMenu.classList.replace("opened_menu", "closed_menu");
+  const menu = document.querySelector('.closed__menu');
+  const newMenu = document.querySelector('.opened__menu');
+  menu.classList.replace('closed__menu', 'opened__menu');
+  newMenu.classList.replace('opened__menu', 'closed__menu');
 
-  const invisible = document.querySelectorAll(".mobile__invisible");
-  const visible = document.querySelectorAll(".mobile__visible");
-  invisible.forEach((tag) =>
-    tag.classList.replace("mobile_invisible", "mobile_visible")
-  );
-  visible.forEach((tag) =>
-    tag.classList.replace("mobile_visible", "mobile_invisible")
-  );
+  const invisible = document.querySelectorAll('.mobile__invisible');
+  const visible = document.querySelectorAll('.mobile__visible');
+  invisible.forEach((tag) => tag.classList.replace('mobile__invisible', 'mobile__visible'));
+  visible.forEach((tag) => tag.classList.replace('mobile__visible', 'mobile__invisible'));
 
-  if (document.querySelector(".toolbar") !== null) {
-    document
-      .querySelector(".toolbar")
-      .classList.replace("toolbar", "toolbar__open");
+  if (document.querySelector('.toolbar') !== null) {
+    document.querySelector('.toolbar').classList.replace('toolbar', 'toolbar__open');
 
-    document
-      .querySelector(".corner_geometry")
-      .classList.add("toolbaropen_row1");
-    document.querySelector(".drop-down").classList.add("toolbar_open_row1");
-    document.querySelector(".mobile_photos").classList.add("toolbaropen_row2");
-    document.querySelector(".desktop_header").classList.add("toolbaropen_row3");
-    document
-      .querySelector(".nav_bottomImage")
-      .classList.add("toolbaropen_row4");
+    document.querySelector('.corner__geometry').classList.add('toolbar__open__row1');
+    document.querySelector('.drop-down').classList.add('toolbar__open__row1');
+    document.querySelector('.mobile__photos').classList.add('toolbar__open__row2');
+    document.querySelector('.desktop__header').classList.add('toolbar__open__row3');
+    document.querySelector('.nav__bottomImage').classList.add('toolbar__open__row4');
   } else {
-    document
-      .querySelector(".toolbar_open")
-      .classList.replace("toolbar_open", "toolbar");
+    document.querySelector('.toolbar__open').classList.replace('toolbar__open', 'toolbar');
 
-    document
-      .querySelector(".corner_geometry")
-      .classList.remove("toolbaropen_row1");
-    document.querySelector(".drop-down").classList.remove("toolbar_open_row1");
-    document
-      .querySelector(".mobile_photos")
-      .classList.remove("toolbaropen_row2");
-    document
-      .querySelector(".desktop_header")
-      .classList.remove("toolbaropen_row3");
-    document
-      .querySelector(".nav_bottomImage")
-      .classList.remove("toolbaropen_row4");
+    document.querySelector('.corner__geometry').classList.remove('toolbar__open__row1');
+    document.querySelector('.drop-down').classList.remove('toolbar__open__row1');
+    document.querySelector('.mobile__photos').classList.remove('toolbar__open__row2');
+    document.querySelector('.desktop__header').classList.remove('toolbar__open__row3');
+    document.querySelector('.nav__bottomImage').classList.remove('toolbar__open__row4');
   }
 }
 
-hamburger.addEventListener("click", () => {
-  const menu = document.querySelector(".closed__menu");
-  const newMenu = document.querySelector(".opened__menu");
-  menu.classList.replace("closed_menu", "opened_menu");
-  newMenu.classList.replace("opened_menu", "closed_menu");
+hamburger.addEventListener('click', () => {
+  const menu = document.querySelector('.closed__menu');
+  const newMenu = document.querySelector('.opened__menu');
+  menu.classList.replace('closed__menu', 'opened__menu');
+  newMenu.classList.replace('opened__menu', 'closed__menu');
 
-  const invisible = document.querySelectorAll(".mobile__invisible");
-  const visible = document.querySelectorAll(".mobile__visible");
-  invisible.forEach((tag) =>
-    tag.classList.replace("mobile_invisible", "mobile_visible")
-  );
-  visible.forEach((tag) =>
-    tag.classList.replace("mobile_visible", "mobile_invisible")
-  );
+  const invisible = document.querySelectorAll('.mobile__invisible');
+  const visible = document.querySelectorAll('.mobile__visible');
+  invisible.forEach((tag) => tag.classList.replace('mobile__invisible', 'mobile__visible'));
+  visible.forEach((tag) => tag.classList.replace('mobile__visible', 'mobile__invisible'));
 
-  if (document.querySelector(".toolbar") !== null) {
-    document
-      .querySelector(".toolbar")
-      .classList.replace("toolbar", "toolbar__open");
-    document
-      .querySelector(".corner_geometry")
-      .classList.add("toolbaropen_row1");
-    document.querySelector(".drop-down").classList.add("toolbar_open_row1");
-    document.querySelector(".mobile_photos").classList.add("toolbaropen_row2");
-    document.querySelector(".desktop_header").classList.add("toolbaropen_row3");
-    document
-      .querySelector(".nav_bottomImage")
-      .classList.add("toolbaropen_row4");
+  if (document.querySelector('.toolbar') !== null) {
+    document.querySelector('.toolbar').classList.replace('toolbar', 'toolbar__open');
+    document.querySelector('.corner__geometry').classList.add('toolbar__open__row1');
+    document.querySelector('.drop-down').classList.add('toolbar__open__row1');
+    document.querySelector('.mobile__photos').classList.add('toolbar__open__row2');
+    document.querySelector('.desktop__header').classList.add('toolbar__open__row3');
+    document.querySelector('.nav__bottomImage').classList.add('toolbar__open__row4');
   } else {
-    document
-      .querySelector(".toolbar_open")
-      .classList.replace("toolbar_open", "toolbar");
+    document.querySelector('.toolbar__open').classList.replace('toolbar__open', 'toolbar');
 
-    document
-      .querySelector(".corner_geometry")
-      .classList.remove("toolbaropen_row1");
-    document.querySelector(".drop-down").classList.remove("toolbar_open_row1");
-    document
-      .querySelector(".mobile_photos")
-      .classList.remove("toolbaropen_row2");
-    document
-      .querySelector(".desktop_header")
-      .classList.remove("toolbaropen_row3");
-    document
-      .querySelector(".nav_bottomImage")
-      .classList.remove("toolbaropen_row4");
+    document.querySelector('.corner__geometry').classList.remove('toolbar__open__row1');
+    document.querySelector('.drop-down').classList.remove('toolbar__open__row1');
+    document.querySelector('.mobile__photos').classList.remove('toolbar__open__row2');
+    document.querySelector('.desktop__header').classList.remove('toolbar__open__row3');
+    document.querySelector('.nav__bottomImage').classList.remove('toolbar__open__row4');
   }
 });
 
 links.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener('click', () => {
     invisibleToogle();
   });
 });
 
-const projectsInfo = [
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/one.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/two.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/three.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/four.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/five.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
-  {
-    name: "Project name goes here",
-    stack: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-    url: "./images/six.svg",
-    descriptionOne:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi",
-    descriptionTwo:
-      "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-  },
+const projectsInfo = [{
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/one.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+}, {
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/two.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+}, {
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/three.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+}, {
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/four.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+}, {
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/five.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+}, {
+  name: 'Project name goes here',
+  stack: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  url: './images/six.svg',
+  descriptionOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  , quis nostrud exercitation ullamco laboris nisi',
+  descriptionTwo: 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+},
 ];
 
-const projectsButtons = Array.from(
-  document.querySelectorAll(".project__button")
-);
+const projectsButtons = Array.from(document.querySelectorAll('.project__button'));
 const maxProjects = projectsButtons.length - 1;
-const navigateToRightGalley = document.querySelector(".pop_display_right");
-const navigateToRight = document.querySelector(".nav_pop_next");
-const navigateToLeftGallery = document.querySelector(".pop_display_left");
-const navigateToLeft = document.querySelector(".nav_pop_prev");
-const smallImages = Array.from(document.querySelectorAll(".small__image"));
+const navigateToRightGalley = document.querySelector('.pop__display__right');
+const navigateToRight = document.querySelector('.nav__pop__next');
+const navigateToLeftGallery = document.querySelector('.pop__display__left');
+const navigateToLeft = document.querySelector('.nav__pop__prev');
+const smallImages = Array.from(document.querySelectorAll('.small__image'));
 let projectIndex = 0;
 let imageIndex = 0;
 
 const loadModel = (i) => {
-  const h1 = document.querySelector(".pop__title");
-  const pictures = document.querySelectorAll(".image__project");
-  const pOne = document.querySelector(".text_pop_one");
-  const pTwo = document.querySelector(".text_pop_two");
+  const h1 = document.querySelector('.pop__title');
+  const pictures = document.querySelectorAll('.image__project');
+  const pOne = document.querySelector('.text__pop__one');
+  const pTwo = document.querySelector('.text__pop__two');
 
   h1.innerHTML = projectsInfo[i].name;
   pictures.forEach((picture) => {
@@ -196,31 +134,31 @@ const loadModel = (i) => {
   projectIndex = i;
 };
 const selectImage = (i = 0) => {
-  const prvs = document.querySelector(".select__image");
+  const prvs = document.querySelector('.select__image');
   if (prvs !== null) {
-    prvs.classList.remove("select__image");
+    prvs.classList.remove('select__image');
   }
-  smallImages[i].classList.add("select__image");
+  smallImages[i].classList.add('select__image');
 };
 projectsButtons.forEach((button, i) => {
-  button.addEventListener("click", () => {
-    popUp.style.display = "grid";
-    sections.forEach((section) => section.classList.add("pop__hide"));
-    main.classList.replace("main", "main__pop");
+  button.addEventListener('click', () => {
+    popUp.style.display = 'grid';
+    sections.forEach((section) => section.classList.add('pop__hide'));
+    main.classList.replace('main', 'main__pop');
 
     loadModel(i);
     selectImage();
   });
 });
 
-navigateToLeftGallery.addEventListener("click", () => {
+navigateToLeftGallery.addEventListener('click', () => {
   if (imageIndex > 0) {
     imageIndex -= 1;
     selectImage(imageIndex);
   }
 });
 
-navigateToLeft.addEventListener("click", () => {
+navigateToLeft.addEventListener('click', () => {
   if (projectIndex > 0) {
     projectIndex -= 1;
     imageIndex = 0;
@@ -229,14 +167,14 @@ navigateToLeft.addEventListener("click", () => {
   }
 });
 
-navigateToRightGalley.addEventListener("click", () => {
+navigateToRightGalley.addEventListener('click', () => {
   if (imageIndex < 3) {
     imageIndex += 1;
     selectImage(imageIndex);
   }
 });
 
-navigateToRight.addEventListener("click", () => {
+navigateToRight.addEventListener('click', () => {
   if (projectIndex < maxProjects) {
     projectIndex += 1;
     imageIndex = 0;
@@ -245,10 +183,10 @@ navigateToRight.addEventListener("click", () => {
   }
 });
 
-closePopUp.addEventListener("click", () => {
-  sections.forEach((section) => section.classList.remove("pop__hide"));
-  main.classList.replace("main__pop", "main");
+closePopUp.addEventListener('click', () => {
+  sections.forEach((section) => section.classList.remove('pop__hide'));
+  main.classList.replace('main__pop', 'main');
 
-  popUp.style.display = "none";
+  popUp.style.display = 'none';
   imageIndex = 0;
 });
